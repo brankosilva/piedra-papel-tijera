47f872a642dac093772722afac9778cb15137693
Branko <brankoemmanuelsilva@gmail.com>
2024-08-24 13:21:11 -0300

    js del juego

---
+++ main.js
@@ -0,0 +1,53 @@
+alert ("Bienvenido al PIEDRA, PAPEL O TIJERA");
+alert ("Para poder iniciar el juego voy a pedirte algunos datos")
+
+/*
+
+// ataque jugador 
+
+let jugador = prompt ("Elegí tu ataque, escribí:\n0 para PIEDRA⚙\n1 para PAPEL📃\n2 para TIJERA✂\nESC para salir");
+
+if ( parseInt(jugador) == 0) {
+    alert ("Elegiste Piedra ⚙")
+} else if ( parseInt(jugador) == 1) {
+    alert ("Elegiste Papel 📃")
+} else if ( parseInt(jugador) == 2) {
+    alert ("Elegiste Tijera ✂")
+} else if ( parseInt(jugador) !== 0 && parseInt(jugador) !== 1 && parseInt(jugador) !== 2 && jugador !== "ESC") {
+    alert ("Eso no es un ataque 😅")
+}
+
+//ataque oponente
+
+let oponente = Math.round(Math.random() *2)
+
+if ( parseInt(oponente) == 0) {
+    alert ("Tu oponente eligió Piedra ⚙")
+} else if ( parseInt(oponente) == 1) {
+    alert ("Tu oponente eligió Papel 📃")
+} else if ( parseInt(oponente) == 2) {
+    alert ("Tu oponente eligió Tijera ✂")
+} else if ( parseInt(oponente) !== 0 && parseInt(oponente) !== 1 && parseInt(oponente) !== 2 && oponente !== "ESC") {
+    alert ("Eso no es un ataque 😅")
+}
+
+// Resultados
+
+if (parseInt(jugador) == parseInt(oponente)) {
+    alert ("Empataron 😮")
+} else if ( parseInt(jugador) == 0 && parseInt(oponente) == 2) {
+    alert ("Ganasteee 🎉🎉")
+}  else if ( parseInt(jugador) == 1 && parseInt(oponente) == 0) {
+    alert ("Ganasteee 🎉🎉")
+} else if ( parseInt(jugador) == 2 && parseInt(oponente) == 1) {
+    alert ("Ganasteee 🎉🎉")
+} else {
+    alert ("Perdiste 😭")
+}
+*/
+
+/**************** JUEGO CON DO....WHILE ******************/
+/*
+do {
+
+} while (jugador !== "ESC")*/
